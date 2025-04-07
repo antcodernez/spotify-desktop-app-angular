decbb277edbd8cadf19dbd51d9631c50d84a1974
Antonio Jiménez <marshday5@gmail.com>
2025-04-07 10:36:30 -0600

    fix(UI): error in main-view containers

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,11 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
-  imports: [],
+  imports: [NgClass],
+  standalone: true,
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css'
 })
 export class MainViewComponent {
   @Input() isLibraryExpanded: boolean = false;
+  @Input() isShowPlaying: boolean = true;
 }
